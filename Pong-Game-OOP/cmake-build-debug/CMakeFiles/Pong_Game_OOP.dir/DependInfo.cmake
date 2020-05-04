3f37b9b70fcfabb0c25d28c6fca3691f12b278df
christianrivero <ch.riverov10@gmail.com>
2020-05-03 19:20:39 -0500

    Creathe the Course Project in CLion IDE

---
+++ Pong-Game-OOP/cmake-build-debug/CMakeFiles/Pong_Game_OOP.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/christian/Repos/Programacion-Orientada-a-Objetos-con-CPP_UTEC-TECSUP/Pong-Game-OOP/main.cpp" "/home/christian/Repos/Programacion-Orientada-a-Objetos-con-CPP_UTEC-TECSUP/Pong-Game-OOP/cmake-build-debug/CMakeFiles/Pong_Game_OOP.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Pong-Game-OOP/cmake-build-debug/CMakeFiles/Pong_Game_OOP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pong_Game_OOP.dir/main.cpp.o"
+  "Pong_Game_OOP"
+  "Pong_Game_OOP.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Pong_Game_OOP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
